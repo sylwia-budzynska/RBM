41dbc6674a2b57c3d4d497127e7c7bbdd26f5c5a
Universal Studio <uni@un1.pw>
2021-01-29 09:56:11 +0100

    feat: trampoline improvements
    now properly applying texture on top face,
    has scaffolding bounding box behavior

--- src/main/java/eu/tmuniversal/rbm/common/block/TrampolineBlock.java
+++ src/main/java/eu/tmuniversal/rbm/common/block/TrampolineBlock.java
@@ -1,5 +1,7 @@
 package eu.tmuniversal.rbm.common.block;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SlimeBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -8,6 +10,10 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.IBlockReader;
 
@@ -15,6 +21,7 @@ public class TrampolineBlock extends SlimeBlock {
 
   public static final double WALKING_MODIFIER = 0.85D;
   public static final SoundEvent SLIME_SOUND_EVENT = SoundEvents.BLOCK_SLIME_BLOCK_STEP;
+  private static final VoxelShape SCAFFOLDING_SHAPE;
 
   public TrampolineBlock() {
     super(ModBlocks.makeBlockProperties(Material.CLAY, MaterialColor.GRASS)
@@ -33,7 +40,7 @@ public class TrampolineBlock extends SlimeBlock {
       Vector3d motion = entityIn.getMotion();
       if (motion.y < -0.1D) {
         double modifier = entityIn instanceof LivingEntity ? 1.35D : 0.8D;
-        entityIn.playSound(SLIME_SOUND_EVENT, ((float) Math.abs(motion.y) / 5), 1.0F);
+        entityIn.playSound(SLIME_SOUND_EVENT, ((float) Math.abs(motion.y) / 6), 1.0F);
         entityIn.setMotion(motion.x, -motion.y * modifier, motion.z);
       }
       else {
@@ -41,5 +48,28 @@ public class TrampolineBlock extends SlimeBlock {
         entityIn.setMotion(entityIn.getMotion().mul(1.0, 0.0, 1.0));
       }
     }
+  }
+
+  @Override
+  public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+    if (!context.hasItem(state.getBlock().asItem())) {
+      return SCAFFOLDING_SHAPE;
+    } else {
+      return VoxelShapes.fullCube();
+    }
+  }
+
+  @Override
+  public VoxelShape getRaytraceShape(BlockState state, IBlockReader worldIn, BlockPos pos) {
+    return VoxelShapes.fullCube();
+  }
+
+  static {
+    VoxelShape voxelshape = Block.makeCuboidShape(0.0D, 14.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+    VoxelShape voxelshape1 = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 2.0D, 16.0D, 2.0D);
+    VoxelShape voxelshape2 = Block.makeCuboidShape(14.0D, 0.0D, 0.0D, 16.0D, 16.0D, 2.0D);
+    VoxelShape voxelshape3 = Block.makeCuboidShape(0.0D, 0.0D, 14.0D, 2.0D, 16.0D, 16.0D);
+    VoxelShape voxelshape4 = Block.makeCuboidShape(14.0D, 0.0D, 14.0D, 16.0D, 16.0D, 16.0D);
+    SCAFFOLDING_SHAPE = VoxelShapes.or(voxelshape, voxelshape1, voxelshape2, voxelshape3, voxelshape4);
   }
 }
